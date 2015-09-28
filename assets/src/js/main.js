523c6fc432951d412bee31e20de484025a972e9a
Marko Manojlovic <marko@ivyexec.com>
2015-09-28 22:10:31 +0200

    Adding animation to students & news cards

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,4 +5,7 @@ $(function() {
         $('.logo').hide();
         //this method increases the height to 72px
     });
+    // Bit of animation
+    // This demo uses drag.
+    $(".col-md-8 .col-md-6").velocity("transition.slideLeftIn", { stagger: 250 });
 });
